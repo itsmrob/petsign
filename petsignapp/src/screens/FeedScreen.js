e74d0b826e2bee5aa560b8a1b0aa1e21c9ee4806
Roberto <Robert_jhs@hotmail.com>
2023-10-31 07:44:45 -0600

    Adding useFetch custom hook to reuse the logic through the app

--- petsignapp/src/screens/FeedScreen.js
+++ petsignapp/src/screens/FeedScreen.js
@@ -1,58 +1,62 @@
-import React from "react";
-import { View, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
 import ReportCard from "../components/feedComponents/ReportCard";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useNavigation } from "@react-navigation/native";
 
+import { useFetchReports } from "../hooks/useFetchReports";
+
 const FeedScreen = () => {
+    const { reports, loading } = useFetchReports();
+
     // Array de datos falsos para simular reportes
-    const reportesFalsos = [
-        {
-            petName: "Fido",
-            petBreed: "Labrador Retriever",
-            petColor: "Amarillo",
-            lastSeenLocation: "123 Calle Principal",
-            lastSeenDate: "2023-10-05",
-            photoURL:
-                "https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg",
-        },
-        {
-            petName: "Buddy",
-            petBreed: "Golden Retriever",
-            petColor: "Dorado",
-            lastSeenLocation: "456 Calle Secundaria",
-            lastSeenDate: "2023-10-06",
-            photoURL:
-                "https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_1280.jpg",
-        },
-        {
-            petName: "Max",
-            petBreed: "Bulldog",
-            petColor: "Marrón",
-            lastSeenLocation: "789 Calle Terciaria",
-            lastSeenDate: "2023-10-07",
-            photoURL:
-                "https://cdn.pixabay.com/photo/2016/03/28/10/05/kitten-1285341_1280.jpg",
-        },
-        {
-            petName: "Luna",
-            petBreed: "Husky Siberiano",
-            petColor: "Gris y blanco",
-            lastSeenLocation: "101 Calle Cuaternaria",
-            lastSeenDate: "2023-10-08",
-            photoURL:
-                "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
-        },
-        {
-            petName: "Rocky",
-            petBreed: "Dóberman",
-            petColor: "Negro y marrón",
-            lastSeenLocation: "202 Calle Quinternaria",
-            lastSeenDate: "2023-10-09",
-            photoURL:
-                "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
-        },
-    ];
+    // const reportesFalsos = [
+    //     {
+    //         petName: "Fido",
+    //         petBreed: "Labrador Retriever",
+    //         petColor: "Amarillo",
+    //         lastSeenLocation: "123 Calle Principal",
+    //         lastSeenDate: "2023-10-05",
+    //         photoURL:
+    //             "https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg",
+    //     },
+    //     {
+    //         petName: "Buddy",
+    //         petBreed: "Golden Retriever",
+    //         petColor: "Dorado",
+    //         lastSeenLocation: "456 Calle Secundaria",
+    //         lastSeenDate: "2023-10-06",
+    //         photoURL:
+    //             "https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_1280.jpg",
+    //     },
+    //     {
+    //         petName: "Max",
+    //         petBreed: "Bulldog",
+    //         petColor: "Marrón",
+    //         lastSeenLocation: "789 Calle Terciaria",
+    //         lastSeenDate: "2023-10-07",
+    //         photoURL:
+    //             "https://cdn.pixabay.com/photo/2016/03/28/10/05/kitten-1285341_1280.jpg",
+    //     },
+    //     {
+    //         petName: "Luna",
+    //         petBreed: "Husky Siberiano",
+    //         petColor: "Gris y blanco",
+    //         lastSeenLocation: "101 Calle Cuaternaria",
+    //         lastSeenDate: "2023-10-08",
+    //         photoURL:
+    //             "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
+    //     },
+    //     {
+    //         petName: "Rocky",
+    //         petBreed: "Dóberman",
+    //         petColor: "Negro y marrón",
+    //         lastSeenLocation: "202 Calle Quinternaria",
+    //         lastSeenDate: "2023-10-09",
+    //         photoURL:
+    //             "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
+    //     },
+    // ];
 
     const navigation = useNavigation();
 
@@ -60,10 +64,16 @@ const FeedScreen = () => {
         navigation.navigate("ReportScreen");
     };
 
+    if (loading) {
+        <View style={styles.loadingStatus}>
+            <ActivityIndicator />
+        </View>
+    }
+
     return (
         <View style={styles.container}>
             <ScrollView>
-                {reportesFalsos.map((reporte, index) => (
+                {reports.map((reporte, index) => (
                     <ReportCard
                         key={index}
                         petName={reporte.petName}
@@ -71,7 +81,7 @@ const FeedScreen = () => {
                         petColor={reporte.petColor}
                         lastSeenlocation={reporte.lastSeenLocation}
                         lastSeenDate={reporte.lastSeenDate}
-                        photoURL={reporte.photoURL}
+                        photoURL={reporte.petImage}
                     />
                 ))}
             </ScrollView>
@@ -87,6 +97,10 @@ const FeedScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    loadingStatus: {
+        justifyContent: 'center',
+        alignItems:'center'
     },
     addButton: {
         position: "absolute",
